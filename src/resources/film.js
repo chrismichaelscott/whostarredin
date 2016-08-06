56df287f062c3a95f4373903cf7262e92b961976
Chris Scott <chris.scott@odinsol.com>
2016-08-06 10:33:09 +0100

    Require config

--- src/resources/film.js
+++ src/resources/film.js
@@ -1,3 +1,4 @@
+var config = require('../../config');
 var Mustache = require('mustache');
 var entityService = require('../services/entity');
 var inliningService = require('../services/inlining');
