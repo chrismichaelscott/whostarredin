c9007cdcd0865e84667fa907576e868d8e6a772d
Ubuntu <ubuntu@ip-172-31-32-239.eu-west-1.compute.internal>
2016-08-16 08:17:58 +0000

    Returned a promise in actor loader (as no post-processing)

--- data-processor-v2/actor/src/elasticsearch.js
+++ data-processor-v2/actor/src/elasticsearch.js
@@ -2,8 +2,9 @@ var config = require('../config');
 var axios = require('axios');
 
 var storeCount = 0;
+
 function storeActor(film) {
-  axios.put(config.elasticsearch.url + "/" + config.elasticsearch.index + "/actor/" + film.model.id, film.model).catch(function(error){
+  return axios.put(config.elasticsearch.url + "/" + config.elasticsearch.index + "/actor/" + film.model.id, film.model).catch(function(error){
     console.error(error);
   }).then(function() {
     storeCount++;
